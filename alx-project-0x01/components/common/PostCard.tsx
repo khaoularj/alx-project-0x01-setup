d1ceac4ec856b9baffec01aefd62f4bb3e74b4af
khaoularj <khaoula.raja1@gmail.com>
2024-12-19 12:39:31 +0000

    updating task0

---
+++ alx-project-0x01/components/common/PostCard.tsx
@@ -0,0 +1,10 @@
+const PostCard: React.FC = () => {
+    return (
+      <div className="border p-4 rounded shadow">
+        <h2 className="text-xl font-bold">Post Title</h2>
+      </div>
+    );
+  };
+  
+  export default PostCard;
+  
