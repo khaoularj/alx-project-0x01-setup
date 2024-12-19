474a29722a1ee28cceba5c5c625f66380cd6def4
khaoularj <khaoula.raja1@gmail.com>
2024-12-19 12:35:05 +0000

    Set up navigation between pages

---
+++ alx-project-0x01/pages/posts/index.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/layout/Header";
+const Posts: React.FC = () => {
+    return (
+      <div>
+        <Header />
+        <main className="p-4">
+          <h1>Posts Page</h1>
+        </main>
+      </div>
+    );
+  };
+  
+  export default Posts;
---
+++ alx-project-0x01/pages/users/index.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/layout/Header";
+const Users: React.FC = () => {
+    return (
+      <div>
+        <Header />
+        <main className="p-4">
+          <h1>Users Page</h1>
+        </main>
+      </div>
+    );
+  };
+  
+  export default Users;
